240b5abec45b45409f6fdd881893f74e22c3049d
Fireye <codekai16@gmail.com>
2025-08-27 22:30:13 -0700

    dash and jumps

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -4,10 +4,12 @@ using System.Collections.Generic;
 
 public partial class Player : CharacterBody2D {
     public const float Speed = 300.0f;
-    public const float JumpVelocity = -400.0f;
+    public const float JumpVelocity = -200.0f;
 
     public AnimationPlayer anim;
     public Timer timer;
+
+    public bool pointingRight = true;
 
     // BOON Handlers
     public int getTotalDoubleJumps() {
@@ -20,20 +22,22 @@ public partial class Player : CharacterBody2D {
         return 0;
     }
 
+    public int totalDoubleJumps;
+
     public int jumpsLeft;
 
-    public bool dashEnabled() {
+    public bool getDashEnabled() {
         HashSet<string> bs = GameState.GetGSInstance().boons;
         return bs.Contains("Dash");
     }
-
     public bool canDash;
 
     public bool dashing;
 
     public override void _Ready() {
         jumpsLeft = getTotalDoubleJumps();
-        canDash = dashEnabled();
+        totalDoubleJumps = getTotalDoubleJumps();
+        canDash = getDashEnabled();
         anim = GetNode<AnimationPlayer>("%Anim");
         timer = GetNode<Timer>("%Timer");
     }
@@ -42,36 +46,61 @@ public partial class Player : CharacterBody2D {
     public override void _PhysicsProcess(double delta) {
         Vector2 velocity = Velocity;
 
+        // Reset double jumps on floor
+        if (jumpsLeft < totalDoubleJumps && IsOnFloor()) {
+            jumpsLeft = totalDoubleJumps;
+        }
+
         // Add the gravity.
         if (!IsOnFloor()) {
             velocity += GetGravity() * (float)delta;
         }
 
         // Handle Jump.
-        if (Input.IsActionJustPressed("jump") && IsOnFloor()) {
+        if (Input.IsActionJustPressed("jump") &&
+            (IsOnFloor() || jumpsLeft > 0)) {
+            if (!IsOnFloor()) {
+                jumpsLeft -= 1;
+            }
             velocity.Y = JumpVelocity;
         }
 
-        // Get the input direction and handle the movement/deceleration.
-        // As good practice, you should replace UI actions with custom gameplay
-        // actions.
-        Vector2 direction = Input.GetVector("left", "right", "up", "down");
-        if (direction != Vector2.Zero) {
-            velocity.X = direction.X * Speed;
+        if (dashing) {
+            if (pointingRight) {
+                velocity.X = 2 * Speed;
+            } else {
+                velocity.X = -2 * Speed;
+            }
         } else {
-            velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+            // Get the input direction and handle the movement/deceleration.
+            // As good practice, you should replace UI actions with custom
+            // gameplay actions.
+            Vector2 direction = Input.GetVector("left", "right", "up", "down");
+            if (direction != Vector2.Zero) {
+                velocity.X = direction.X * Speed;
+                pointingRight = velocity.X > 0;
+            } else {
+                velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+            }
         }
 
         Velocity = velocity;
         MoveAndSlide();
     }
 
-    public void dash() {
-        if (canDash) {
-            dashing = true;
-        }
+    // Handle special inputs
+    public override void _Input(InputEvent @event) {
         // TODO: If cannot dash, communicate to player
+        if (@event.IsActionPressed("dash") && canDash) {
+            if (pointingRight) {
+                anim.Play("Dash");
+            } else {
+                anim.Play("Dash_Left");
+            }
+        }
     }
+
+    public void dash() { dashing = true; }
 
     public void endDash() {
         dashing = false;
